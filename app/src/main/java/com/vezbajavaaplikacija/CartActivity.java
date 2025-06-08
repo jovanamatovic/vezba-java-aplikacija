6ddf0590cc11b7a5f1ae7ca8414d8fbbd6caade6
Jovana Matovic <jmatovic77@gmail.com>
2025-06-08 18:20:53 +0200

    izmene MainActivity i CartActivity

--- app/src/main/java/com/vezbajavaaplikacija/CartActivity.java
+++ app/src/main/java/com/vezbajavaaplikacija/CartActivity.java
@@ -14,14 +14,10 @@ import java.util.Map;
 
 public class CartActivity extends AppCompatActivity {
 
-    // kontejner u kojem se nalaze proizvodi dodati u korpu
     LinearLayout cartContainer;
-    // sumirana cena svih proizvoda u korpi
     TextView totalSum;
-    // dugme za potvrdjivanje kupovine
     Button btnConfirmOrder;
 
-    // kreiranje mape za cenu proizvoda u korpi
     Map<Integer, Double> prices = new HashMap<>();
 
     @Override
@@ -33,7 +29,6 @@ public class CartActivity extends AppCompatActivity {
         totalSum = findViewById(R.id.totalSum);
         btnConfirmOrder = findViewById(R.id.btnConfirmOrder);
 
-        // dodavanje slike i cene proizvoda u mapu
         prices.put(R.drawable.ball, 15.99);
         prices.put(R.drawable.ball2, 16.99);
         prices.put(R.drawable.shoes, 49.99);
@@ -41,10 +36,8 @@ public class CartActivity extends AppCompatActivity {
         prices.put(R.drawable.shorts, 19.99);
         prices.put(R.drawable.shorts2, 25.99);
 
-        // automatsko refreshovanje kartice svaki put kada se doda novi proizvod ili ukloni
         refreshCart();
 
-        // posle klika, samo obrise proizvode iz korpe i vrati na pocetnu stranu
         btnConfirmOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,37 +47,27 @@ public class CartActivity extends AppCompatActivity {
         });
     }
 
-    // funkcija za refreshovanje korpe svaki put kada se nesto doda ili ukloni iz nje
     private void refreshCart() {
 
-        // ovo se koristi da bi mogla lista da se rebuilduje opet na osnovu stanja korpe
         cartContainer.removeAllViews();
 
-        // ova vrednost prati ukupnu cenu svih proizvoda u korpi, i updatuje se automatski kada se nesto doda ili ukloni
         double total = 0.0;
 
-        // for petlja koja prolazi kroz svaki proizvod u korpi, svaki proizvod ima svoj imageId**
         for (int i = 0; i < MainActivity.cartItems.size(); i++) {
             int imageId = MainActivity.cartItems.get(i);
 
-            // proverava cenu svakog proizvoda u korpi na osnovu imageId
-            // getOrDefaul samo osigurava da ukoliko se vrednost ne nalazi u mapi, cena ce mu biti 0.0 (to se u ovom slucaju nece desiti nikad)
             total += prices.getOrDefault(imageId, 0.0);
 
-            // kreira novi view na osnovu cart_item.xml u /res/layout
             View cartItem = LayoutInflater.from(this).inflate(R.layout.cart_item, cartContainer, false);
-            // reference iz cart_item.xml
             ImageView itemImage = cartItem.findViewById(R.id.itemImage);
             TextView itemPrice = cartItem.findViewById(R.id.itemPrice);
             Button btnRemove = cartItem.findViewById(R.id.btnRemove);
 
-            // postavlja image i cenu za svaki proizvod
             itemImage.setImageResource(imageId);
             itemPrice.setText(String.format("$%.2f", prices.getOrDefault(imageId, 0.0)));
 
 
             int finalI = i;
-            // klikom se brise proizvod iz korpe i automatski poziva refreshCart() funkciju da se i na ekranu prikaze promena
             btnRemove.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -93,11 +76,9 @@ public class CartActivity extends AppCompatActivity {
                 }
             });
 
-            // ovde se dodaje cart_item layout na ekran
             cartContainer.addView(cartItem);
         }
 
-        // ovde se totalSum vrednost updatuje na osnovu cene proizvoda u korpi
         totalSum.setText(String.format("Total: $%.2f", total));
     }
 }
--- app/src/main/java/com/vezbajavaaplikacija/MainActivity.java
+++ app/src/main/java/com/vezbajavaaplikacija/MainActivity.java
@@ -16,11 +16,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    // kontejner za sve proizvode na main stranici
     LinearLayout itemContainer;
-    // dugme za prelaz na stranicu za prikaz korpe
     Button btnGoToCart;
-    // array lista svih proizvoda za korpu
     public static ArrayList<Integer> cartItems = new ArrayList<>();
 
     @Override
@@ -31,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
         itemContainer = findViewById(R.id.itemContainer);
         btnGoToCart = findViewById(R.id.btnGoToCart);
 
-        // slike za sve proizvode u aplikaciji (lopta, patike i sorc)
-        // nalaze se u /res/drawable
         int[] images = {
                 R.drawable.ball,
                 R.drawable.ball2,
@@ -42,9 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.shorts2
         };
 
-        // hash mapa za cenu svakog proizvoda pojedinacno
         Map<Integer, String> prices = new HashMap<>();
-        // dodavanje cene za svaki proizvod
         prices.put(R.drawable.ball, "$15.99");
         prices.put(R.drawable.ball2, "$16.99");
         prices.put(R.drawable.shoes, "$49.99");
@@ -52,13 +45,10 @@ public class MainActivity extends AppCompatActivity {
         prices.put(R.drawable.shorts, "$19.99");
         prices.put(R.drawable.shorts2, "$25.99");
 
-        // for petlja koja iterira kroz svaki imageId u images listi i za svaki imageId uzima odredjenu cenu svakog proizvoda
         for (int imageId : images) {
-            // poziva addItemToLayout funkciju koja prikazuje proizvode na main stranici
             addItemToLayout(imageId, prices.get(imageId));
         }
 
-        // klikom na ovaj button se prelazi na korpa stranicu uz pomoc ** startActivity i Intent**
         btnGoToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,22 +57,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // funkcija koja prikazuje proizvode na stranici
     private void addItemToLayout(int imageId, String price) {
 
-        // ovde se **inflatuje** main_item.xml
-        // to je u prevodu custom item layout kojim se lakse dizajniraju same kartice za proizvode na stranici
-        // nalazi se u /res/layout
         View itemView = LayoutInflater.from(this).inflate(R.layout.main_item, itemContainer, false);
         ImageView itemImage = itemView.findViewById(R.id.itemImage);
         Button addToCart = itemView.findViewById(R.id.btnAddToCart);
         TextView itemPrice = itemView.findViewById(R.id.itemPrice);
 
-        // postavlja sliku i cenu za svaki proizvod
         itemImage.setImageResource(imageId);
         itemPrice.setText(price);
 
-        // kada se ovo dugme klikne, imageId kliknutog proizvoda se dodaje u globalnu listu **cartItems**, uz pomoc koje se nakon toga prikazuju na korpa stranici
         addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // ovde se doda taj custom main_item.xml u activity_main.xml
         itemContainer.addView(itemView);
     }
 }
